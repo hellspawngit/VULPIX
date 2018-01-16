aac247d9689a402da8e2df2407d5b80b037228be
hellspawngit <34998942+hellspawngit@users.noreply.github.com>
2018-01-16 14:23:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
- console.log(Date.now() + " Just got pinged!");
- response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
- http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
-
 const Discord = require("discord.js");
 const weather = require("weather-js");
 const YTDL = require('ytdl-core');
